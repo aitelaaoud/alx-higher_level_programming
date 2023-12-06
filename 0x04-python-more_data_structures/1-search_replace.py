002a4ba1d012abb3377024e8da6507869e13e28a
aitelaaoud <ahmed.contact.18@gmail.com>
2023-12-06 12:28:54 -0800

    upload

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
-    newlist = [replace if element == search else element for
-            element in my_list]
-    return newlist
+    return [replace if num == search else num for num in my_list]
+
+
+if __name__ == '__main__':
+    my_list = [1, 2, 3, 4, 5]
+    new_list = search_replace(my_list, 2, 89)
+
+    print(new_list)
+    print(my_list)
 
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+
 def uniq_add(my_list=[]):
-    if my_list is None:
-        return None
-    sum = 0
-    newlist = set()
-    for i in my_list:
-        newlist.add(i)
-    for nums in newlist:
-        sum += nums
-    return sum
+    return sum(set(my_list))
+
+
+if __name__ == '__main__':
+    my_list = [1, 2, 3, 1, 4, 2, 5]
+    result = uniq_add(my_list)
+    print("Result: {:d}".format(result))
 
